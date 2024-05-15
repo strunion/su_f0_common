c92d55f9b797e29e0b9e81b730d2a8eb3f40a527
yayayat <mitya.borisov@yandex.ru>
2024-05-15 22:18:53 +0500

    Add startup.s

---
+++ startup_boot.s
@@ -0,0 +1,68 @@
+.syntax unified
+.cpu cortex-m0
+.fpu softvfp
+.thumb
+
+.global g_pfnVectors
+.global Default_Handler
+
+.word _sidata
+.word _sdata
+.word _edata
+.word _sbss
+.word _ebss
+
+.section .text.Reset_Handler
+.weak Reset_Handler
+.type Reset_Handler, %function
+
+Reset_Handler:
+  ldr   r0, =_estack
+  mov   sp, r0
+
+  ldr r0, =_sdata
+  ldr r1, =_edata
+  ldr r2, =_sidata
+  movs r3, #0
+  b LoopCopyDataInit
+
+CopyDataInit:
+  ldr r4, [r2, r3]
+  str r4, [r0, r3]
+  adds r3, r3, #4
+
+LoopCopyDataInit:
+  adds r4, r0, r3
+  cmp r4, r1
+  bcc CopyDataInit
+
+  ldr r2, =_sbss
+  ldr r4, =_ebss
+  movs r3, #0
+  b LoopFillZerobss
+
+FillZerobss:
+  str  r3, [r2]
+  adds r2, r2, #4
+
+LoopFillZerobss:
+  cmp r2, r4
+  bcc FillZerobss
+
+  bl main
+
+.size Reset_Handler, .-Reset_Handler
+
+.section .text.Default_Handler,"ax",%progbits
+Default_Handler:
+Infinite_Loop:
+  b Infinite_Loop
+.size Default_Handler, .-Default_Handler
+
+.section .isr_vector,"a",%progbits
+.type g_pfnVectors, %object
+.size g_pfnVectors, .-g_pfnVectors
+
+g_pfnVectors:
+  .word  _estack
+  .word  Reset_Handler
